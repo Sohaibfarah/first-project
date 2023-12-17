54cf9225fd5ec4f767754417b4df775309157c01
Sohaib <151467772+Sohaibfarah@users.noreply.github.com>
2023-12-17 17:44:28 +0300

    Create snack.js

---
+++ snack.js
@@ -0,0 +1,21 @@
+let listA = document.querySelectorAll('a');
+let active = 'intro';
+let zIndex = 2;
+
+listA.forEach(a => {
+    a.addEventListener('click', function (event) {
+        let valueTab = a.dataset.tab;
+        let activeOld = document.querySelector('.tab.active');
+        if(activeOld) activeOld.classList.remove('active');
+
+        if (valueTab && valueTab != active) {
+            let tabActive = document.getElementById(valueTab);
+            zIndex++;
+            tabActive.style.zIndex = zIndex;
+            active = valueTab;
+            tabActive.style.setProperty('--x', event.clientX + 'px');
+            tabActive.style.setProperty('--y', event.clientY + 'px');
+            tabActive.classList.add('active');
+        }
+    })
+})
